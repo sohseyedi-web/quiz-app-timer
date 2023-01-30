020c7fe212e7a625bf3f51848e761f96cb56f490
sohseyedi <sohseyedi@gmail.com>
2023-01-30 09:31:29 +0330

    start completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
 import Start from './components/Startpage/Start';
+import {useState} from "react";
+import Quiz from "./components/Quiz/Quiz.jsx";
+
 function App() {
 
-  return (
-    <Start/>
-  )
+    const [userName, setUserName] = useState(null)
+
+    return (
+        <>
+            {
+                !userName ? (<Start setUser={setUserName}/>) : (<Quiz/>)
+            }
+        </>
+    )
 }
 
 export default App
--- src/components/Startpage/Start.jsx
+++ src/components/Startpage/Start.jsx
@@ -1,20 +1,30 @@
 import "./Start.scss";
+import {useForm} from "react-hook-form";
 
-const Start = () => {
-  return (
-    <section className="start">
-      <div className="container">
-        <form className="start-form">
-          <h3 className="start-form__title">به کوییز پلاس خوش اومدی</h3>
-          <p className="start-form__subtitle">
-            لطفا اسم خودت را بنویس و مسابقه رو شروع کن
-          </p>
-          <input type="text" placeholder="...." className="start-form__input" />
-          <button className="start-form__btn">شروع مسابقه</button>
-        </form>
-      </div>
-    </section>
-  );
+const Start = ({setUser}) => {
+
+    const {register, handleSubmit, formState: {errors}} = useForm()
+
+    const onSubmit = (data) => {
+        setUser(data)
+    }
+
+    return (
+        <section className="start">
+            <div className="container">
+                <form className="start-form" onSubmit={handleSubmit(onSubmit)}>
+                    <h3 className="start-form__title">به کوییز پلاس خوش اومدی</h3>
+                    <p className="start-form__subtitle">
+                        لطفا اسم خودت را بنویس و مسابقه رو شروع کن
+                    </p>
+                    <input type="text" {...register('userName', { required: true, minLength: 3 })}
+                           className={errors.userName ? "start-form__input error" : "start-form__input"}/>
+                    {errors.userName && <p className={"start-form__error"}>لطفا اسمت رو بگو</p>}
+                    <button className="start-form__btn">شروع مسابقه</button>
+                </form>
+            </div>
+        </section>
+    );
 };
 
 export default Start;
